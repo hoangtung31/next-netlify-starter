868002895470d3b0cf567c2b20a69619d3f84da8
TungHD <>
2023-02-16 13:43:16 +0700

    Update content

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/ic_fox.png" alt="Fox Logo" className={styles.logo} /> for you
+        Made with <img src="/ic_fox.png" alt="Fox Logo" className={styles.logo} /> for you
       </footer>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Welcome to my Next.js site!</title>
+        <link rel="icon" href="/ic_fox.png" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Narcotics Anonymous" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <code>Insanity is doing the same thing, over and over again, but expecting different results.</code>
         </p>
       </main>
 
